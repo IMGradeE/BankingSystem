e1d4907c46607a6c19fce13b406ea1066633a0b8
IMGradeE <wilkespayton@gmail.com>
2024-10-25 14:17:32 -0600

    Fixed an error associated with register_user when attempting to register a user that already exists.

--- Lib/SQL_Statements.js
+++ Lib/SQL_Statements.js
@@ -376,14 +376,18 @@ const DBProcedureStatements = [
             "    end if;\n" +
             "end;", name: "insert_account_type"},
     register_user= { statement: "create procedure if not exists register_user(in externalID int, in unHashedPassword varchar(255),\n" +
-            "                                             in firstName varchar(32),\n" +
-            "                                             in lastName varchar(32), in userRoleID int, out registerSuccess bit(1))\n" +
-            "begin\n" +
-            "    set registerSuccess = "+sqlBool.true+";\n" +
-            "    set @salt = (SELECT SUBSTRING(SHA1(RAND()), 1, 6));\n" +
-            "    insert into users(external_id, hashed_password, salt, first_name, last_name, user_role_id)\n" +
-            "    values (externalID, SHA1(CONCAT(unHashedPassword, @salt)), @salt, firstName, lastName, userRoleID);\n" +
-            "end;", name: "register_user"},
+            "                                                 in firstName varchar(32),\n" +
+            "                                                 in lastName varchar(32), in userRoleID int, out registerSuccess bit(1))\n" +
+            "    begin\n" +
+            "        if externalID not in (select external_id from users) then\n" +
+            "            set registerSuccess = b'1';\n" +
+            "        set @salt = (SELECT SUBSTRING(SHA1(RAND()), 1, 6));\n" +
+            "        insert into users(external_id, hashed_password, salt, first_name, last_name, user_role_id)\n" +
+            "        values (externalID, SHA1(CONCAT(unHashedPassword, @salt)), @salt, firstName, lastName, userRoleID);\n" +
+            "            else\n" +
+            "            set registerSuccess = b'0';\n" +
+            "        end if;\n" +
+            "    end;", name: "register_user"},
     check_credentials= { statement: "create procedure if not exists check_credentials(in externalID int, in unHashedPassword varchar(255),\n" +
             "                                                 out userRoleID int,\n" +
             "                                                 out credentialsAuthorized bit(1))\n" +
